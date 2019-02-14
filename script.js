2aab07480a0e20416725b69648c80fb92134ff6b
Johanna-hub <jyherman@gmail.com>
2019-02-14 12:39:59 +0000

    Iframe test

---
+++ script.js
@@ -0,0 +1,29 @@
+
+//  "Test subframes receive orientation change events"
+
+async function iframe() {
+  const iframe = document.getElementById("testIframe")
+  iframe.requestFullscreen();
+  let orientations = [
+    'portrait-primary',
+    'portrait-secondary',
+    'landscape-primary',
+    'landscape-secondary'
+  ];
+  if (screen.orientation.type.includes('portrait')) {
+    orientations = orientations.reverse();
+  }
+  
+  function log(any){
+    console.log(`${any}`);
+  }
+
+  screen.orientation.addEventListener("message", log(this))
+
+  for (const orientation of orientations) {
+    await screen.orientation.lock(orientation);
+  }
+  screen.orientation.unlock();
+  iframe.exitFullscreen();
+}
+
