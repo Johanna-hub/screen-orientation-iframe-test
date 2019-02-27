4c47673c9b6765c72ac62547b3574f478f99476a
Johanna-hub <jyherman@gmail.com>
2019-02-27 20:06:44 +0000

    Testing

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-//  "Test subframes receive orientation change events"
-
-async function iframe() {
+async function lock() {
   await document.documentElement.requestFullscreen();
-  let orientations = [
-    "portrait-primary",
-    "portrait-secondary",
-    "landscape-primary",
-    "landscape-secondary"
-  ];
-  if (screen.orientation.type.includes("portrait")) {
-    orientations = orientations.reverse();
-  }
-
-  for (const orientation of orientations) {
-    function log(event) {
-      console.log(`${event.data} + ${orientation} should be the same`);
-    }
-    window.addEventListener("message", log);
-    await screen.orientation.lock(orientation);  
+  const currentOrientation = screen.orientation.type;
+  const isPortrait = currentOrientation.includes("portrait");
+  const newOrientation = `${ isPortrait ? "landscape" : "portrait" }-primary`;
+  try {
+    await screen.orientation.lock(newOrientation);
+  } catch (err) {
+    console.log(`main doc error: ${err}`)
   }
   screen.orientation.unlock();
   return document.exitFullscreen();
