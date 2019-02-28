19b33bbf6194ba630f0280ecdbce916a0672154e
Johanna-hub <jyherman@gmail.com>
2019-02-28 19:42:58 +0000

    New iframe code

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ async function lock() {
     console.log(`main doc error: ${err}`)
   }
 }
+
+async function iframeLock(){
+  const iframe = window.frames['testIframe'];
+  await iframe.requestFullscreen();
+  const iframeOrientation = iframe.contentWindow.screen.orientation.type;
+  const isPortrait = iframeOrientation.includes("portrait");
+  const newOrientation = `${ isPortrait ? "landscape" : "portrait" }-primary`;
+  try {
+    await iframe.contentWindow.screen.orientation.lock(newOrientation);
+    console.log(`iframe orientation is ${iframeWindow.screen.orientation.type}`)
+  } catch (err) {
+    console.log(`iframe error: ${err}`)
+  }
+}
+
+window.onload = lock();
