68cb2c028fb63dfb82f4cac1065059e7feccd4ef
Johanna-hub <jyherman@gmail.com>
2019-03-05 20:49:12 +0000

    log iframe error

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-async function lock() {
-  await document.documentElement.requestFullscreen();
-  const currentOrientation = screen.orientation.type;
-  const isPortrait = currentOrientation.includes("portrait");
-  const newOrientation = `${ isPortrait ? "landscape" : "portrait" }-primary`;
-  try {
-    await screen.orientation.lock(newOrientation);
-  } catch (err) {
-    console.log(`main doc error: ${err}`)
-  }
-}
+//  async function bless() {
+//     const b =document.createElement("button")
+//     document.body.appendChild(b);
+//     b.innerText = "CLICK ME! I AM BLESSED!";
+//     b.onclick = document.documentElement.requestFullscreen();
+//     return new Promise(r => b.onclick= r);
+//  }
 
-async function iframeLock(){
-  const iframe = window.frames['testIframe'];
-  await iframe.requestFullscreen();
-  const iframeOrientation = iframe.contentWindow.screen.orientation.type;
-  const isPortrait = iframeOrientation.includes("portrait");
-  const newOrientation = `${ isPortrait ? "landscape" : "portrait" }-primary`;
-  try {
-    await iframe.contentWindow.screen.orientation.lock(newOrientation);
-    console.log(`iframe orientation is ${iframe.contentWindow.screen.orientation.type}`)
-  } catch (err) {
-    console.log(`iframe error: ${err}`)
-  }
-}
-
-// window.onload = lock();
